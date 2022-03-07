f56820a510d7470977921814a78ad1cd9b7cfb23
Ethan Gilworth <49215273+EthanRiley@users.noreply.github.com>
2022-03-07 18:13:38 -0500

    Added priority functionality and wrote goals for agents

--- scheduling.py
+++ scheduling.py
@@ -3,6 +3,7 @@ import pandas as pd
 import random as rnd
 import json
 from collections import defaultdict
+import functools
 
 def setups(L, data):
     return sum([1 for x, y in zip(L, L[1:]) if data[x]['product'] != data[y]['product']])
@@ -11,17 +12,13 @@ def delays(L, data):
     return sum([data[x]['quantity'] for x, y in zip(L, L[1:]) if y < x])
 
 def priority(L, data):
-    # Figure out number of high priority orders
-    # Until every high priority order has been fulfilled, add 1 for every low priority order
-    # Find index of last high priority order
-    num_high_prio = sum([1 for x in L if data[x]['priority'] == 'HIGH'])
-    num_low_prio = sum([1 for x in L if data[x]['priority'] == 'LOW'])
-    #back_list = L[::-1]
-    #end_low_prio = back_list.index()
-    last_high_prio_position = 4
-    return sum([data[L[x]]['quantity'] for x in range(len(L)) if x < last_high_prio_position and data[L[x]]['priority'] == 'LOW'])
-
-    pass
+    # Filter list to list of just high priority orders
+    high_only = list(filter((lambda order: data[order]['priority'] == 'HIGH'), L))
+    # Find out what the last high priority is
+    last = high_only[-1]
+    # Determine position of last high priority order with list index
+    index = L.index(last)
+    return sum([data[L[x]]['quantity'] for x in range(len(L)) if x < index and data[L[x]]['priority'] == 'LOW'])
 
 def swapper(solutions):
     L = solutions[0]
@@ -31,12 +28,15 @@ def swapper(solutions):
     return L
 
 def goal_directed_swapper1(solutions):
+    # Improves setups
     pass
 
 def goal_directed_swapper2(solutions):
+    # Improves by delays
     pass
 
 def goal_directed_swapper3(solutions):
+    # improves by priority
     pass
 
 def main():
